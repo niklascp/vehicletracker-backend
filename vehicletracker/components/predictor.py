e108e44f6b800061426693b38b81b9adb519d3ef
niklascp <niklascp@gmail.com>
2019-09-12 20:09:05 +0200

    Auto-refresh model while running

--- vehicletracker/components/predictor.py
+++ vehicletracker/components/predictor.py
@@ -3,6 +3,7 @@ import os
 import logging
 
 from vehicletracker.helpers.events import EventQueue
+from vehicletracker.helpers.model_store import LocalModelStore
 
 from datetime import datetime
 
@@ -17,47 +18,6 @@ LINK_MODEL_PATH = './cache/lt-link-travel-time/'
 LINK_MODELS = {}
 
 event_queue = EventQueue(domain = 'predictor')
-
-class LocalModelStore():
-    import joblib
-
-    def __init__(self, path):
-        self.path = path
-
-    def load_metadata(self):
-        self.models = []
-
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-
-        for file_name in os.listdir(self.path):
-            if not file_name.endswith(".json"):
-                continue
-
-            metadata_file_path = os.path.join(self.path, file_name)
-
-            with open(metadata_file_path, 'r') as f:
-                model_metadata = json.load(f)
-
-            self.models.append(model_metadata)
-
-    def list_models(self):
-        return self.models
-
-    def load_model(self, model_name):
-        pass
-        #for file_name in os.listdir(MODEL_CACHE_PATH):
-        #    if (file_name.endswith(".json")):
-        #        _LOGGER.info(f'Loading cached model from data from {file_name}')
-        #        metadata_file_path = os.path.join(MODEL_CACHE_PATH, file_name)
-        #        model_file_path = os.path.splitext(os.path.join(MODEL_CACHE_PATH, file_name))[0] + '.joblib'
-        #        
-        #        with open(metadata_file_path, 'r') as f:
-        #            model_metadata = json.load(f)
-        #        with open(model_file_path, 'rb') as f:
-        #            model = joblib.load(f)
-        #        
-        #        LINK_MODELS[model_metadata['linkRef']] = { 'model': model, 'metadata': model_metadata }
 
 link_model_store = LocalModelStore(LINK_MODEL_PATH)
 
@@ -78,8 +38,7 @@ def list_link_models(service_data):
     return link_model_store.list_models()
 
 def link_model_available(event):
-    #link_model_store.refresh(event[''])
-    pass
+    link_model_store.add_model(event['metadata'])
 
 def start(): 
     # TODO: Move into class DiskModelCache
---
+++ vehicletracker/helpers/model_store.py
@@ -0,0 +1,56 @@
+import os
+import joblib
+import json
+
+class LocalModelStore():
+    
+    def __init__(self, path):
+        self.path = path
+
+    def load_metadata(self):
+        self.models = {}
+
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+        for file_name in os.listdir(self.path):
+            if not file_name.endswith(".json"):
+                continue
+
+            metadata_file_path = os.path.join(self.path, file_name)
+
+            with open(metadata_file_path, 'r') as f:
+                model_metadata = json.load(f)
+                self.add_model(model_metadata)
+
+    def add_model(self, model_metadata):
+        if not 'hash' in model_metadata:
+            return
+
+        model_hash_hex = model_metadata['hash']
+        model_hash_simple_len = 6
+
+        while model_hash_hex[:model_hash_simple_len] in self.models:
+            model_hash_simple_len += 1
+
+        model_metadata['hash'] = model_hash_hex[:model_hash_simple_len]
+
+        self.models[model_metadata['hash']] = model_metadata
+
+    def list_models(self):
+        return self.models.values()
+
+    def load_model(self, model_hash):
+        pass
+        #for file_name in os.listdir(MODEL_CACHE_PATH):
+        #    if (file_name.endswith(".json")):
+        #        _LOGGER.info(f'Loading cached model from data from {file_name}')
+        #        metadata_file_path = os.path.join(MODEL_CACHE_PATH, file_name)
+        #        model_file_path = os.path.splitext(os.path.join(MODEL_CACHE_PATH, file_name))[0] + '.joblib'
+        #        
+        #        with open(metadata_file_path, 'r') as f:
+        #            model_metadata = json.load(f)
+        #        with open(model_file_path, 'rb') as f:
+        #            model = joblib.load(f)
+        #        
+        #        LINK_MODELS[model_metadata['linkRef']] = { 'model': model, 'metadata': model_metadata }
