bfd545fb3c872f14f911ed176182eca79c5d2503
niklascp <niklascp@gmail.com>
2019-09-24 14:22:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+import vehicletracker.const as const
+
 PROJECT_NAME = "Vehicle Tracker"
 PROJECT_PACKAGE_NAME = "vehicletracker"
 PROJECT_LICENSE = "Apache License 2.0"
-PROJECT_URL = "https://github.com/niklascp/vehicletracker-backend"
+PROJECT_URL = "https://github.com/niklascp/vehicletracker"
 
 PROJECT_AUTHOR = "Niklas Christoffer Petersen"
 PROJECT_EMAIL = "niklascp@gmail.com"
 
-import vehicletracker.const as const
-
 with open('README.md', 'r') as file:
-    long_description = file.read()
+    LONG_DESCRIPTION = file.read()
 
 with open('requirements.txt') as file:
-    install_requires = [line.rstrip('\r\n') for line in file]
+    INSTALL_REQUIRES = [line.rstrip('\r\n') for line in file]
 
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
@@ -30,7 +30,7 @@ setup(
     packages=PACKAGES,
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    install_requires=INSTALL_REQUIRES,
     #python_requires=">={}".format(MIN_PY_VERSION),
     test_suite="tests",
     entry_points={"console_scripts": ["vehicletracker = vehicletracker.__main__:main"]},
