9f1b8ba52e6098651d0ddfdaf64866eb2bec1758
niklascp <niklascp@gmail.com>
2020-07-06 14:27:07 +0200

    More tests

--- tests/components/trainer/test_trainer.py
+++ tests/components/trainer/test_trainer.py
@@ -3,6 +3,7 @@
 import pytest
 
 from vehicletracker.components import model_registry
+from vehicletracker.components import trainer
 from vehicletracker.core import VehicleTrackerNode, async_setup_component
 
 @pytest.mark.asyncio
@@ -21,7 +22,5 @@ async def test_mock_model():
     node = VehicleTrackerNode(CONFIG) #get_test_home_assistant()
     await node.async_start()
     assert await async_setup_component(node, model_registry.DOMAIN, CONFIG) == True
-
-    assert len(node.data[model_registry.DOMAIN].models) == 1
-    assert 'MockModel' in node.data[model_registry.DOMAIN].models
+    assert await async_setup_component(node, trainer.DOMAIN, CONFIG) == True
     await node.async_stop()
