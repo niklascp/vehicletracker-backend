a61705ad9536b27f780df3cb049f4534420384bd
niklascp <niklascp@gmail.com>
2020-09-16 15:33:10 +0200

    Refactor models.

--- vehicletracker/models/__init__.py
+++ vehicletracker/models/__init__.py
@@ -1,3 +1,17 @@
-from .WeeklyHistoricalAverage import WeeklyHistoricalAverage
-from .WeeklyKernelInterpolation import WeeklyKernelInterpolation
-from .WeeklySvr import WeeklySvr
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, Dict
+
+from vehicletracker.helpers.spatial_reference import SpatialRef
+
+class BaseModel(ABC):
+    def __init__(self, node):
+        self.node = node
+
+    @abstractmethod
+    def model_type(self) -> str:
+        pass
+
+    @abstractmethod
+    def train(self, time : datetime, spatial_ref : SpatialRef, parameters : Dict[str, Any]) -> Dict[str, Any]:
+        pass
